2012b8eccde528d027970b3d20546ac43a635687
shivkumargurjar <theshiv2024@gmail.com>
2025-11-08 17:04:54 +0000

    data typs

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,7 @@
+let name="shiv kumar gurjar";
+let age=21;
+let state;
+console.log(typeof "shiv kumar gurjar")
+console.log(typeof age)
+console.log(typeof state)
+console.log(typeof null)
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=123456
+let accountEmail="shiv@gmail.com"
+var accountPaasword="2234"
+accountCity="jaipur"
+let accountState;
+
+//do  not use var
+accountEmail="gurjar@gmail.com"
+accountPaasword="0000"
+accountCity="sikar"
+console.table([accountId,accountEmail,accountPaasword,accountCity,accountState])
