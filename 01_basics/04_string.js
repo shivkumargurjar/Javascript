6531a465ce719304b949d9a30ea8e2d194abc8df
shivkumargurjar <theshiv2024@gmail.com>
2025-11-09 17:58:31 +0000

    string

---
+++ 01_basics/04_string.js
@@ -0,0 +1,6 @@
+const name="shiv"
+const midname="kumar"
+console.log(name+midname)
+console.log(`hello friend my name is ${name} and my midname is ${midname}`)
+const newName=new String('Rajasthan')
+console.log(newName[0])
