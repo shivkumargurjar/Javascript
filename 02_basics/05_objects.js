ae9c7a3678694870f7a73990fc8974483614cf62
shivkumargurjar <theshiv2024@gmail.com>
2026-01-12 14:39:27 +0000

    api

--- 02_basics/05_objects.js
+++ 02_basics/05_objects.js
@@ -1,2 +1,14 @@
 //object de-structure
 
+const course={
+    coursename:"javascript",
+    courseprice:"999",
+    courseinstructor:"hitesh"
+}
+
+const {courseinstructor}=course
+console.log(courseinstructor)
+
+const {courseinstructor:instructor}=course
+console.log(instructor)
+
