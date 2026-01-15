8dae61ee689c87c331950424bff6a50c3b4b9ed2
shivkumargurjar <theshiv2024@gmail.com>
2026-01-15 14:42:20 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,32 @@
+const value=true;
+if(value){
+    //console.log("yes")
+}
+else{
+    //console.log("NO");
+}
+
+const month=5;
+switch(month){
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Fabruary");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    default:
+        console.log("invalid")
+        break;
+}
